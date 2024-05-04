b922709f09f5a4cb35b0fb4ac55c030731cbff90
KazbundaYTB <gejmraopi11@gmail.com>
2024-05-04 12:34:33 +0200

    Connected to own firebase DB.

--- chat/src/api/firebase.js
+++ chat/src/api/firebase.js
@@ -1,13 +1,24 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection } from "firebase/firestore";
 
+//DENIS LOGIN (KROUZEK)
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDfFIb7Z6k6pQt5QsmIdQ2AgV1Jx8XJ2P0",
+//   authDomain: "schoolchatappkids.firebaseapp.com",
+//   projectId: "schoolchatappkids",
+//   storageBucket: "schoolchatappkids.appspot.com",
+//   messagingSenderId: "190568430302",
+//   appId: "1:190568430302:web:8af203db979e17d2b0204f",
+// };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDfFIb7Z6k6pQt5QsmIdQ2AgV1Jx8XJ2P0",
-  authDomain: "schoolchatappkids.firebaseapp.com",
-  projectId: "schoolchatappkids",
-  storageBucket: "schoolchatappkids.appspot.com",
-  messagingSenderId: "190568430302",
-  appId: "1:190568430302:web:8af203db979e17d2b0204f",
+  apiKey: "AIzaSyDYTmqPnuqG_k0vBA5UwDohXcHluQKlUl0",
+  authDomain: "chatappdb-5cad4.firebaseapp.com",
+  projectId: "chatappdb-5cad4",
+  storageBucket: "chatappdb-5cad4.appspot.com",
+  messagingSenderId: "1000459507260",
+  appId: "1:1000459507260:web:bcdfa1149d38dd701c5539",
+  measurementId: "G-WSGFSKNT19"
 };
 
 export const app = initializeApp(firebaseConfig);
