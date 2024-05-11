1665b2f9713390294113e1da062c3c194f505d82
Kazbunda <gejmraopi11@gmail.com>
2024-05-11 22:34:40 +0200

    Changed system message color, (800 => 600).

--- chat/src/components/chatlist.jsx
+++ chat/src/components/chatlist.jsx
@@ -5,7 +5,7 @@ export default function ChatList({ arr }) {
   return (
     <div className="w-full h-full flex flex-col justify-end bg-green-100">
       {arr.length === 0 ? (
-        <div className="text-center text-red-800 text-xl font-bold">SYSTÉM - Nebyly nalezeny žádné zprávy k načtení!</div>
+        <div className="text-center text-red-600 text-xl font-bold">SYSTÉM - Nebyly nalezeny žádné zprávy k načtení!</div>
       ) : (
         arr.map((item) => (
           <Message
