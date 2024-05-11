e8baf10538a31381a4c48b96f470c80212731941
Kazbunda <gejmraopi11@gmail.com>
2024-05-11 21:27:02 +0200

    Added system message if no message found.

--- chat/src/components/chatlist.jsx
+++ chat/src/components/chatlist.jsx
@@ -3,16 +3,20 @@ import Message from "./message";
 
 export default function ChatList({ arr }) {
   return (
-    <div className="w-full h-full flex flex-col justify-end bg-green-100 ">
-      {arr.map((item) => (
-        <Message
-          id={item.id}
-          key={item.id}
-          name={item.username}
-          message={item.message}
-          time={item.time}
-        />
-      ))}
+    <div className="w-full h-full flex flex-col justify-end bg-green-100">
+      {arr.length === 0 ? (
+        <div className="text-center text-red-800 text-xl font-bold">SYSTÉM - Nebyly nalezeny žádné zprávy k načtení!</div>
+      ) : (
+        arr.map((item) => (
+          <Message
+            id={item.id}
+            key={item.id}
+            name={item.username}
+            message={item.message}
+            time={item.time}
+          />
+        ))
+      )}
     </div>
   );
 }
