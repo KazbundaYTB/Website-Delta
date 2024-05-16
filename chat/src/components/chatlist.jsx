ab8aa56c8c28c36e5aaf635431f6a85da848f7c4
Kazbunda <gejmraopi11@gmail.com>
2024-05-16 15:29:46 +0200

    Added chatlist scroll bar. Now you can read old messages

--- chat/src/components/chatlist.jsx
+++ chat/src/components/chatlist.jsx
@@ -4,6 +4,7 @@ import Message from "./message";
 export default function ChatList({ arr }) {
   return (
     <div className="w-full h-full flex flex-col justify-end bg-green-100">
+      <div className=" overflow-y-scroll">
       {arr.length === 0 ? (
         <div className="text-center text-red-600 text-xl font-bold">SYSTÉM - Nebyly nalezeny žádné zprávy k načtení!</div>
       ) : (
@@ -17,6 +18,7 @@ export default function ChatList({ arr }) {
           />
         ))
       )}
+    </div>
     </div>
   );
 }
