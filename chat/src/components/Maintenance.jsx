32f0a53916c5ee45a94112bd762e86b410a0d5e6
Kazbunda <gejmraopi11@gmail.com>
2024-05-18 16:08:47 +0200

    Added Discord button to maintenance screen

--- chat/src/components/Maintenance.jsx
+++ chat/src/components/Maintenance.jsx
@@ -7,7 +7,7 @@ export default function Maintenance({ setMaintenanceStatus, username }) {
 
   return (
     <div className="w-screen h-screen flex items-center bg-slate-600 flex-col space-y-2.5 justify-center">
-      <div className="text-center text-white text-3xl">Maintenance</div>
+      <div className="text-center text-white text-3xl">Under Maintenance!</div>
       <br />
       <div className="p-4 text-xl rounded-lg flex items-center text-white bg-slate-600 flex-col space-y-5 justify-center">
         <h1>This website is under maintenance! Check our discord for more info or try it again later!</h1>
@@ -17,6 +17,9 @@ export default function Maintenance({ setMaintenanceStatus, username }) {
               Continue with Maintenance bypass
             </button>
           )}
+          <button className="px-4 py-2 bg-blue-500 text-white rounded items-center">
+              <a href="https://localhost:3000/">Discord</a>
+            </button>
         </div>
       </div>
     </div>
